ff2ce3ba419507747d4103dd41b0133739ffaf35
Dan Tang <dan@stats4sd.org>
2026-05-27 13:05:33 +0100

    Admin Panel > Teams > Users tab page, add permission checking for Invite users button and Add Existing User to team button

---
+++ app/Filament/Admin/Resources/TeamResource/RelationManagers/UsersRelationManager.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Filament\Admin\Resources\TeamResource\RelationManagers;
+
+use Awcodes\Shout\Components\Shout;
+use Filament\Forms;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Stats4sd\FilamentTeamManagement\Filament\Admin\Resources\TeamResource\RelationManagers\UsersRelationManager as BaseUsersRelationManager;
+
+class UsersRelationManager extends BaseUsersRelationManager
+{
+    public function table(Table $table): Table
+    {
+        return parent::table($table)
+            ->headerActions([
+                Tables\Actions\Action::make('invite users')
+                    ->form([
+                        Shout::make('info')
+                            ->type('info')
+                            ->content('Add the email address(es) of the user(s) you would like to invite to this team. An invitation will be sent to each address.')
+                            ->columnSpanFull(),
+                        Forms\Components\Repeater::make('users')
+                            ->label('Email Addresses to Invite')
+                            ->simple(
+                                Forms\Components\TextInput::make('email')
+                                    ->email()
+                                    ->required()
+                            )
+                            ->reorderable(false)
+                            ->addActionLabel('Add Another Email Address'),
+                    ])
+                    ->visible(fn () => auth()->user()->can('maintain teams'))
+                    ->action(function (array $data, RelationManager $livewire) {
+                        if (!auth()->user()->can('maintain teams')) {
+                            abort(403);
+                        }
+
+                        $this->handleInvitation($data, $livewire->getOwnerRecord());
+                    }),
+                Tables\Actions\AttachAction::make()
+                    ->label('Add Existing User to team')
+                    ->visible(fn () => auth()->user()->can('maintain teams')),
+            ]);
+    }
+}
