aa43d35c9cb11556dbb5e6744a1a415895c5823e
Ciara <ciara@stats4sd.org>
2026-05-29 12:16:49 +0100

    extracts instructions

--- resources/views/filament/app/pages/data-collection/set-up-survey.blade.php
+++ resources/views/filament/app/pages/data-collection/set-up-survey.blade.php
@@ -4,25 +4,7 @@ use App\Filament\App\Pages\SurveyDashboard;
 
 $surveyDashboardUrl = SurveyDashboard::getUrl();
 
-$instructions = <<<'EOT'
-Once you have tested and finalised the details of your localised HOLPA survey, data collection may begin.
-
-##### Set up live forms for data collection
-
-Up until this point, all the forms used for pilot testing have been labelled as "test" forms, and the submissions are stored as test data, not to be included in the results. To begin actual data collection, you need to use this section to set your survey to "live". There are some notes on this page to prompt you to double check all the necessary tasks have been completed. Read through these, and when you are ready, click the button to make your survey live.
-
-##### Access live forms
-
-Once your survey is live, this section will display the QR code to set up new devices with the correct forms. Enumerators who have already joined the project using the QR code at the pilot phase can alternatively sync their devices to receive the updated forms.
-
-Before enumerators commence data collection, they should double check that the forms on their ODK Collect app are indeed the live versions; test versions will be labelled as such in the form title, e.g. "HOLPA Household Form - Local Shared Test Version". Reminder: under no circumstances should enumerators use the draft versions of the survey forms accessed from the initial pilot section. The data from these is not saved and will be lost.
-
-Once enumerators begin data collection, you will be able to see form submissions in the next section, Monitor data collection.
-
-##### Forms overview
-
-Here you can see the titles and published status of your forms. There are options here to publish changes if this still needs to be done before commencing data collection.
-EOT;
+$instructions = tfile('setup-survey');
 
 ?>
 <x-filament-panels::page class="px-10 h-full">
