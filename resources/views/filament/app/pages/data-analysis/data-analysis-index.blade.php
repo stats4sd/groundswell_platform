1cedb4e7366b5bd51098a4a6b6fad93719aeb01e
Dave Mills <d.e.mills@stats4sd.org>
2026-06-01 17:36:18 -0400

    hotfix - analysis page now uses correct config

--- resources/views/filament/app/pages/data-analysis/data-analysis-index.blade.php
+++ resources/views/filament/app/pages/data-analysis/data-analysis-index.blade.php
@@ -17,7 +17,7 @@ $instructions = tfile('data-analysis');
     </x-instructions-sidebar>
     <div class="mx-0 xl:px-4" style="margin-top:-50px">
         <div class="surveyblocks">
-            <x-shiny-loader::shiny-iframe shiny-app-url="http://127.0.0.1:7009" :post-data="['foo' => 'bar']" />
+            <x-shiny-loader::shiny-iframe shiny-app-url="{{ config('shiny-loader.analysis-app-url') }}" :post-data="['foo' => 'bar']" />
         </div>
     </div>
 
