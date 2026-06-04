e31f2d9ea5e8b3ef59576b99c33408fb6388e6d6
Dan Tang <dan@stats4sd.org>
2026-06-04 14:05:14 +0100

    add new custom page Optional Modules to show xlsform module versions without xlsform_module_id and owner_id

---
+++ app/Filament/App/Clusters/Localisations/Pages/OptionalModules.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\App\Clusters\Localisations\Pages;
+
+use App\Filament\App\Clusters\Localisations;
+use Filament\Navigation\NavigationItem;
+use Filament\Pages\Page;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
+use Stats4sd\FilamentOdkLink\Models\OdkLink\XlsformModuleVersion;
+
+class OptionalModules extends Page implements HasTable
+{
+    use InteractsWithTable;
+
+    protected static ?string $cluster = Localisations::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-puzzle-piece';
+
+    protected static ?string $navigationLabel = 'Optional Modules';
+
+    protected static ?int $navigationSort = 3;
+
+    protected static string $view = 'filament.app.clusters.localisations.pages.optional-modules';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                XlsformModuleVersion::query()
+                    ->whereNull('xlsform_module_id')
+                    ->whereNull('owner_id')
+            )
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                IconColumn::make('is_default')
+                    ->boolean()
+                    ->label('Default Version'),
+                TextColumn::make('survey_rows_count')
+                    ->counts('surveyRows')
+                    ->label('# Questions'),
+            ])
+            ->paginated(false);
+    }
+}
