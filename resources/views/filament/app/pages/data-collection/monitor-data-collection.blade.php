3b979ae8ea887504e0840a4ca106c16c278a3116
Dave Mills <d.e.mills@stats4sd.org>
2026-06-18 17:15:30 +0100

    remove debug

--- resources/views/filament/app/pages/data-collection/monitor-data-collection.blade.php
+++ resources/views/filament/app/pages/data-collection/monitor-data-collection.blade.php
@@ -9,9 +9,6 @@
     </x-instructions-sidebar>
     <div class="mx-0 xl:px-4" style="margin-top:-50px">
         <div class="surveyblocks">
-            <pre>
-                @dump($shinyData)
-            </pre>
 
             <x-shiny-loader::shiny-iframe shiny-app-url="{{ config('shiny-loader.monitoring-app-url') }}"
                 :post-data="$shinyData->toArray()" />
