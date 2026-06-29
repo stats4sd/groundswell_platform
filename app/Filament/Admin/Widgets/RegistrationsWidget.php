06fcc820a73ac8bdbe2ca8b4cf42cd4974be9922
Dave Mills <d.e.mills@stats4sd.org>
2026-06-29 10:11:09 +0100

    remove custom stat widget blade: - revert to Filament default widgets for compatability

--- app/Filament/Admin/Widgets/RegistrationsWidget.php
+++ app/Filament/Admin/Widgets/RegistrationsWidget.php
@@ -2,10 +2,10 @@
 
 namespace App\Filament\Admin\Widgets;
 
-use App\Filament\Admin\Widgets\StatsOverviewWidget\Stat;
 use App\Models\Team;
 use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Support\HtmlString;
 use Stats4sd\FilamentTeamManagement\Models\Program;
 
