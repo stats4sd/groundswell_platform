dc6c110f8892fb3844062b903396ae0040389b11
Dave Mills <d.e.mills@stats4sd.org>
2026-07-22 15:11:59 +0100

    refactor: consume single-server shiny-loader config

--- config/shiny-loader.php
+++ config/shiny-loader.php
@@ -1,12 +1,14 @@
 <?php
 
-// config for  Stats4sd/LaravelShinyLoader
+// config for Stats4sd/LaravelShinyLoader
 return [
-    'app-path' => env('SHINY_APP_PATH', base_path('shiny')),
-    'auth-key' => env('SHINY_AUTH_KEY', 'change-me'),
+    'root_url' => env('SHINY_ROOT_URL', 'http://localhost:3838'),
+    'root_path' => env('SHINY_ROOT_PATH', '/srv/shiny-server'),
+    'auth_key' => env('SHINY_AUTH_KEY', 'change-me'),
 
-    ## Add the urls to every shiny app that will be embedded here, and the .env KEYs that hold them, e.g.:
-    'monitoring-app-url' => env('SHINY_APP_URL_MONITORING', 'http://127.0.0.1:7007'),
-    'analysis-app-url' => env('SHINY_APP_URL_ANALYSIS', 'http://127.0.0.1:7009'),
-
+    // Folder names of the shiny apps on the shiny server; each is served at {root_url}/{name}/.
+    'apps' => [
+        'groundswell_monitor',
+        'groundswell_analysis',
+    ],
 ];
